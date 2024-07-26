38122c0e69e0156b1ec9f6b056f722949a5356bd
nagaraju Vuppala <vpp.nagaraju1234@gmail.com>
2024-07-26 16:36:43 +0530

    mmts status

---
+++ mmts/js/config.js
@@ -0,0 +1,9 @@
+var config = {
+    //add more annaphora_types in below list, each item separated by comma
+    //Before colon is the one displayed on screen, after colon is the one stored internally as value
+    annaphora_types: [
+        'Annaphora_C:annaphora_c', 
+        'Annaphora_RC:annaphora_rc'
+    ],
+    font_sizes : ['1:1', "2:2", '3:3', '4:4', '5:5', '6:6', '7:7', '8:8', '9:9', '10:10']
+};
---
+++ mmts/js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+	document.querySelectorAll('[data-bs-toggle="tooltip"]')
+	.forEach(tooltip => {
+	  new bootstrap.Tooltip(tooltip)
+	});
+	var intervalId = window.setInterval(function(){
+		// call your function here
+		loadLiveStatus();
+	  }, 30000);
+	
+});
+
+function loadLiveStatus(){
+	console.log("iam jere");
+	$.ajax({
+		url: "http://mmts.geektheory.in/config/livetrain.php",
+		type: 'GET',
+		// data: formData,
+		async: false,
+		cache: false,
+		contentType: false,
+		processData: false,
+		success: function (data) {
+			console.log(data);
+			const keys = Object.keys(data);
+			var sno = 1;
+			var table = $('<table></table>').addClass("sortable table table-striped table-bordered dt-responsive nowrap'").attr("id", "statsTable");
+					var th = $( '<tr><th>SNO</th><th>Train No</th><th>From</th><th>To</th><th>Last Location</th>' ).appendTo(table);
+			for (let i = 0; i < keys.length; i++) {
+				const key = keys[i];
+				console.log(key, data[key]);
+				var train_no = key;
+				if(data[train_no] != null && "details" in  data[train_no] ) {
+					var islive = data[train_no]["details"]["islive"];
+					console.log(islive);
+					
+					var last_stn =  data[train_no]["details"]["laststn"];
+					var start =  data[train_no]["details"]["startstnname"];
+					var to = data[train_no]["details"]["endstnname"];
+					console.log(train_no);
+					console.log(last_stn);
+					
+					if(islive == "true") {
+						var tr = $( '<tr></tr>' ).appendTo(table);
+						$('<td width="10%">' + sno + '</td>').appendTo(tr);
+						$('<td width="10%">' + train_no + '</td>').appendTo(tr);
+						$('<td width="20%">' + start + '</td>').appendTo(tr);
+						$('<td width="20%">' + to + '</td>').appendTo(tr);
+						$('<td width="20%">' + last_stn + '</td>').appendTo(tr);
+					}
+				}
+			}
+			$("#statusTable").html(table);
+
+		}, 
+		error:function  (jqXHR, exception) {
+			$("#loading").hide();
+			var msg = '';
+			if (jqXHR.status === 0) {
+					msg = 'Not connect.\n Verify Network.';
+			} else if (jqXHR.status == 404) {
+					msg = 'Requested page not found. [404]';
+			} else if (jqXHR.status == 500) {
+					msg = 'Internal Server Error [500].';
+			} else if (exception === 'parsererror') {
+					msg = 'Requested JSON parse failed.';
+			} else if (exception === 'timeout') {
+					msg = 'Time out error.';
+			} else if (exception === 'abort') {
+					msg = 'Ajax request aborted.';
+			} else {
+					msg = 'Uncaught Error.\n' + jqXHR.responseText;
+			}
+			alert("Error in File conversion "+msg);
+		}
+	});
+
+}
