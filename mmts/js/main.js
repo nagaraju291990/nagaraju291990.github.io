739afa8b9af9bd58e1b28c3fed610a3749dd29fc
nagaraju Vuppala <vpp.nagaraju1234@gmail.com>
2024-07-30 16:36:23 +0530

    some styles

--- mmts/js/main.js
+++ mmts/js/main.js
@@ -26,7 +26,9 @@ function loadLiveStatus(){
 			const keys = Object.keys(data);
 			var sno = 1;
 			var table = $('<table></table>').addClass("sortable table table-striped table-bordered dt-responsive nowrap'").attr("id", "statsTable");
+			// var table = $('<div></div>').addClass('').css("border-collapse", "collapse").css("margin", "3px").attr('id', 'statsTable');
 			var th = $( '<tr><th>SNO</th><th>Train No</th><th>From</th><th>To</th><th>Last Location</th><th>SD</th><th>AD</th>' ).appendTo(table);
+			// var th = $('<tr><th>SNO</th><th>Train No</th><th>From</th><th>To</th><th>Last Location</th><th>SD</th><th>AD</th>').addClass("divrow nodoubt");
 			for (let i = 0; i < keys.length; i++) {
 				const key = keys[i];
 				console.log(key, data[key]);
@@ -44,12 +46,22 @@ function loadLiveStatus(){
 					console.log(last_stn);
 					
 					if(islive == "true") {
+						
 						var tr = $( '<tr></tr>' ).appendTo(table);
-						$('<td width="5%">' + sno + '</td>').appendTo(tr);
+						$('<td width="1%">' + sno + '</td>').appendTo(tr);
 						$('<td width="7%">' + train_no + '</td>').appendTo(tr);
 						$('<td width="10%">' + start + '</td>').appendTo(tr);
 						$('<td width="10%">' + to + '</td>').appendTo(tr);
 						$('<td width="20%">' + last_stn + '</td>').appendTo(tr);
+
+						// tr = $('<div></div>').addClass("divrow nodoubt");
+						// var colnum = $('<div>' + sno + '</div>').addClass("title col-num").appendTo(tr);
+						// var col_trainno = $('<div>' + train_no + '</div>').addClass("title col-num").appendTo(tr);
+						// var colsrc = $('<div>' + start + '</div>').addClass("title srcdiv").appendTo(tr);
+						// var coltgt = $('<div>' + to + '</div>').addClass("title tgtdiv").appendTo(tr);
+						// var col_last_stn = $('<div>' + last_stn + '</div>').addClass("title srcdiv").appendTo(tr);
+
+						
 						var sd = '';
 						var ed = '';
 						if("StopsNumber" in data[train_no]["details"]) {
@@ -63,6 +75,9 @@ function loadLiveStatus(){
 						}
 						$('<td width="5%">' + sd + '</td>').appendTo(tr);
 						$('<td width="5%">' + ed + '</td>').appendTo(tr);
+						// var coltgt = $('<div>' + sd + '</div>').addClass("title tgtdiv").appendTo(tr);
+						// var col_last_stn = $('<div>' + ed + '</div>').addClass("title savetitle").appendTo(tr);
+						// table.append(tr);
 						sno = sno + 1;
 					}
 				}
